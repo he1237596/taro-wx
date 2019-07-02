f86e841187b01a4ff36fee7347d1ef8c2811d403
Chris <raoxc@mogulinker.com>
2019-07-03 02:07:28 +0800

    删除redux文件夹

--- src/utils/dva.js
+++ src/utils/dva.js
@@ -1,5 +1,5 @@
 import { create } from 'dva-core';
-// import { createLogger } from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import createLoading from 'dva-loading';
 
 let app;
@@ -8,7 +8,7 @@ let dispatch;
 
 function createApp(opt) {
   // redux日志
-  // opt.onAction = [createLogger()];
+  opt.onAction = [createLogger()];
   app = create(opt);
   app.use(createLoading({}));
 
